f5d3c42af9b5fd4e4aa4ded6f29c484c3c37d986
Zerokoska <vasulhavruliv1.c@gmail.com>
2024-11-08 12:15:41 +0100

    Merged main into vasyl

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import "./App.css";
 
 import Homepage from "./Homepage";
 import AuthorsInfo from "./AuthorsInfo";
-import AuthorsInfo from "./AuthorsInfo";
 
 function App() {
   return (
     <>
       <Homepage />
-      <AuthorsInfo />
       <AuthorsInfo />
     </>
   );
