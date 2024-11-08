6f104b588cb3a2383a1c55326e92fbce42a33ceb
Zerokoska <vasulhavruliv1.c@gmail.com>
2024-11-08 12:08:02 +0100

    author components crated

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 
 import Homepage from "./Homepage";
 import AuthorsInfo from "./AuthorsInfo";
-
+import AuthorsInfo from "./AuthorsInfo";
 
 function App() {
   return (
     <>
       <Homepage />
       <AuthorsInfo />
- 
+      <AuthorsInfo />
     </>
   );
 }
